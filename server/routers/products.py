507cd82878fbfc915d4c134d34701cbf4afe5da6
psihoshlem <yungync@gmail.com>
2023-09-19 21:58:37 +0300

    searching

--- server/routers/products.py
+++ server/routers/products.py
@@ -4,7 +4,8 @@ from fastapi import APIRouter, Depends
 from functions.jwt_funcs import jwt_bearer
 from functions.db_products_funcs import (
     get_product,
-    get_all_products
+    get_all_products,
+    get_products
 )
 
 
@@ -19,3 +20,7 @@ async def get_all_products_info():
 @router.get("/get_product", tags=["products"])
 async def get_one_product(user = Depends(jwt_bearer), id: int = 1):
     return get_product(id)
+
+@router.get("/search", tags=["products"])
+async def search_product(query):
+    return get_products(query)
